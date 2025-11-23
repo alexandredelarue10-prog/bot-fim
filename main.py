a1518ac05f8ab370ce2bf025ca53dffa91749c3d
alexandredelarue10-prog <alexandredelarue10@gmail.com>
2025-11-23 12:10:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,6 @@ PREFIX = "!"
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix=PREFIX, intents=intents, help_command=None)
-bot = commands.Bot(command_prefix="!", help_command=None)
 
 
 DB_NAME = "bot_data.sqlite"
